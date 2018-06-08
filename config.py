0ae1c730178b7e8a6a49245c2126903ed342932f
Patrick Lee <patrick.lee@u-psud.fr>
2018-06-08 15:53:23 +0200

    modified binds

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ if os.environ.get('DATABASE_URL') is None:
 else:
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_BINDS = {
-        'origin':    './FLpredictorapp/origin.db',
-        'dest':      './FLpredictorapp/dest.db'
+        'origin':    './origin.db',
+        'dest':      './dest.db'
     }
     DATABASE_URI = './FLpredictorapp/static/db/'
